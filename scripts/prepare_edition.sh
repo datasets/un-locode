750e8615bc31f00b56bdd7ed8cc2f4c96e22f4c3
Stefano <sabas88@gmail.com>
2024-02-16 10:17:47 +0100

    Uniq in subdivision table

--- scripts/prepare_edition.sh
+++ scripts/prepare_edition.sh
@@ -12,6 +12,7 @@ done
 gawk -v RS='"[^"]*"' -v ORS= '{gsub(/\n/, " ", RT); print $0  RT}' mdb_SubdivisionCodes_out.csv > subdivision-codes.tmp.csv
 sed -i 's/ \{2,\}/ /g;s/[[:blank:]]*$//' subdivision-codes.tmp.csv
 sed -i 's/\t/ /g' subdivision-codes.tmp.csv
+uniq subdivision-codes.tmp.csv > subdivision-codes.tmp2.csv
 
 gawk -v RS='"' 'NR % 2 == 0 { gsub(/\n/, "") } { printf("%s%s", $0, RT) }'  "mdb_${filename}_out.csv" > code-list.tmp.csv #remove newlines in data
 csvgrep -c 3 -r "^$" -i code-list.tmp.csv > code-list.tmp2.csv #remove country headers
@@ -22,5 +23,5 @@ mv code-list.csv data/code-list.csv
 mv mdb_CountryCodes_out.csv data/country-codes.csv
 mv mdb_FunctionClassifiers_out.csv data/function-classifiers.csv
 mv mdb_StatusIndicators_out.csv data/status-indicators.csv
-mv subdivision-codes.tmp.csv data/subdivision-codes.csv
+mv subdivision-codes.tmp2.csv data/subdivision-codes.csv
 rm *.csv
